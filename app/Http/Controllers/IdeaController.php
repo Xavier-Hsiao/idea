b336d69179b3a4469ad32cfc2f9d36d6516b48a5
Xavier <tim88776@gmail.com>
2026-02-20 22:15:51 +0800

    refactor: implement idea controllers

---
+++ app/Http/Controllers/IdeaController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Idea;
+use Illuminate\Http\Request;
+
+class IdeaController extends Controller
+{
+	/**
+	 * Display a listing of ideas.
+	 */
+	public function index()
+	{
+		$ideas = Idea::all();
+
+		return view("ideas.index", [
+			"ideas" => $ideas,
+		]);
+	}
+
+	/**
+	 * Show the form for creating a new idea.
+	 */
+	public function create()
+	{
+		return view("ideas.create");
+	}
+
+	/**
+	 * Store a newly created idea in storage.
+	 */
+	public function store(Request $request)
+	{
+		$idea = $request->input("description");
+
+		Idea::create([
+			"description" => $idea,
+			"state" => "pending",
+		]);
+
+		return redirect("/ideas");
+	}
+
+	/**
+	 * Display the specified idea.
+	 */
+	public function show(Idea $idea)
+	{
+		return view("ideas.show", [
+			"idea" => $idea,
+		]);
+	}
+
+	/**
+	 * Show the form for editing the specified idea.
+	 */
+	public function edit(Idea $idea)
+	{
+		return view("ideas.edit", [
+			"idea" => $idea,
+		]);
+	}
+
+	/**
+	 * Update the specified idea in storage.
+	 */
+	public function update(Request $request, Idea $idea)
+	{
+		$idea->update([
+			"description" => $request->input("description"),
+		]);
+
+		return redirect("/ideas/{$idea->id}");
+	}
+
+	/**
+	 * Remove the specified idea from storage.
+	 */
+	public function destroy(Idea $idea)
+	{
+		$idea->delete();
+
+		return redirect("/ideas");
+	}
+}
--- routes/web.php
+++ routes/web.php
@@ -1,63 +1,12 @@
 <?php
 
-use Illuminate\Support\Facades\Request;
+use App\Http\Controllers\IdeaController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Idea;
 
-// show all ideas
-Route::get("/ideas", function () {
-	$ideas = Idea::all();
-
-	return view("ideas.index", [
-		"ideas" => $ideas,
-	]);
-});
-
-// get the create form
-Route::get("/ideas/create", function () {
-	return view("ideas.create");
-});
-
-// store a new idea
-Route::post("/ideas", function () {
-	$idea = Request::input("description");
-
-	Idea::create([
-		"description" => $idea,
-		"state" => "pending",
-	]);
-
-	return redirect("/ideas");
-});
-
-// show an idea
-Route::get("/ideas/{idea}", function (Idea $idea) {
-	// $idea = Idea::findOrFail($id)
-
-	return view("ideas.show", [
-		"idea" => $idea,
-	]);
-});
-
-// get the edit form
-Route::get("/ideas/{idea}/edit", function (Idea $idea) {
-	return view("ideas.edit", [
-		"idea" => $idea,
-	]);
-});
-
-// update an idea
-Route::patch("/ideas/{idea}", function (Idea $idea) {
-	$idea->update([
-		"description" => request("description"),
-	]);
-
-	return redirect("/ideas/{$idea->id}");
-});
-
-// destroy an idea
-Route::delete("/ideas/{idea}", function (Idea $idea) {
-	$idea->delete();
-
-	return redirect("/ideas");
-});
+Route::get("/ideas", [IdeaController::class, "index"]);
+Route::get("/ideas/create", [IdeaController::class, "create"]);
+Route::post("/ideas", [IdeaController::class, "store"]);
+Route::get("/ideas/{idea}", [IdeaController::class, "show"]);
+Route::get("/ideas/{idea}/edit", [IdeaController::class, "edit"]);
+Route::patch("/ideas/{idea}", [IdeaController::class, "update"]);
+Route::delete("/ideas/{idea}", [IdeaController::class, "destroy"]);
