ccac9b0ba14907f9a9e9b178ece344a145b416e7
Xavier <tim88776@gmail.com>
2026-02-20 15:06:12 +0800

    filter ideas by state using Eloquent

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,16 @@
 <?php
 
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Idea;
 
 Route::get("/", function () {
 	// $ideas = session()->get("ideas", []);
+	// $ideas = DB::table("ideas")->get();
 
-	$ideas = DB::table("ideas")->get();
+	$ideas = Idea::when(request("state"), function ($query, $state) {
+		$query->where("state", $state);
+	})->get();
 
 	return view("ideas", [
 		"ideas" => $ideas,
@@ -17,7 +20,12 @@ Route::get("/", function () {
 Route::post("/ideas", function () {
 	$idea = Request::input("idea");
 
-	session()->push("ideas", $idea);
+	Idea::create([
+		"description" => $idea,
+		"state" => "pending",
+	]);
+
+	// session()->push("ideas", $idea);
 
 	return redirect("/");
 });
